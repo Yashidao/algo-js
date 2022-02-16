de930e8815335859e79f9ef13b0e55afc94dc038
Tran Kevin <trankevinmay@gmail.com>
2022-02-16 14:53:41 +0100

    bonus

--- js/Introduction/1.8.js
+++ js/Introduction/1.8.js
@@ -1,7 +1,11 @@
 const readlineSync = require("readline-sync");
 
+
 let x = readlineSync.question("Give me your favorite animal: ");
+console.log("C'est la première question, il est reste 2 autres.")
 let y = readlineSync.question("Give me your age: ");
+console.log("Il est reste 1 question. :)")
 let z = readlineSync.question("Give me your favorite drink: ");
+console.log("C'est fini, voici une petite histoire!")
 
 console.log("In a another dimension, you serve your " + x + " a " + z + " but beware if you fail to do that, your " + x + " will punish you with " + y + " punchs!");
