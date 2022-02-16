6118bb6bc0825d1575105bbda7b7e4a1241eff3f
Tran Kevin <trankevinmay@gmail.com>
2022-02-16 12:20:19 +0100

    exercice n1 fini

---
+++ js/test.js
@@ -0,0 +1,8 @@
+const readlineSync = require("readline-sync");
+
+let name = "Jean";
+let nameSister = "Emma";
+console.log("My name is " + name);
+
+let userName = readlineSync.question("What's your name?");
+console.log("He's called " + userName);
